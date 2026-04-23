57e356bccb8e86e91212ed707c43687c9abcbd27
Unathi-Sibanda <Unathi.Sibanda@khonology.com>
2026-04-23 15:00:13 +0200

    UI: remove blue accent border from non-admin sidebar

--- frontend_flutter/lib/widgets/app_side_nav.dart
+++ frontend_flutter/lib/widgets/app_side_nav.dart
@@ -107,6 +107,9 @@ class _AppSideNavState extends State<AppSideNav> {
     final items = widget.items ??
         (widget.isAdmin ? AppSideNav._adminItems : AppSideNav._items);
 
+    final leftBorderColor =
+        widget.isAdmin ? AppSideNav.leftAccentColor : Colors.transparent;
+
     final collapsedWidth =
         widget.collapsedWidthOverride ?? AppSideNav.collapsedWidth;
     final expandedWidth =
@@ -127,7 +130,7 @@ class _AppSideNavState extends State<AppSideNav> {
       decoration: BoxDecoration(
         color: chrome.sidebarBackground,
         border: Border(
-          left: const BorderSide(color: AppSideNav.leftAccentColor, width: 3),
+          left: BorderSide(color: leftBorderColor, width: 3),
           right: BorderSide(color: chrome.sidebarRightBorder, width: 1),
         ),
       ),
